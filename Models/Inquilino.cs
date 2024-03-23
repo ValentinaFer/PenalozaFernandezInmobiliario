af29d304f483dfc6d6bbead765a1af18b9218679
ValentinaFer <quevedoants@gmail.com>
2024-03-23 03:12:51 -0300

    Created Inquilino Model

--- Models/Inquilino.cs
+++ Models/Inquilino.cs
@@ -1,6 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PenalozaFernandezInmobiliario.Models;
 
 public class Inquilino
 {
-    
+    [Key]
+    [Display(Name = "Código Int.")]
+    public int Id { get; set;}
+    [Required]
+    public string? Nombre{ get; set;}
+    [Required]
+    public string? Apellido { get; set;}
+    [Required]
+    public string? Email { get; set;}
+    [Required]
+    [Display(Name = "Teléfono")]
+    public string? Telefono { get; set;}
+    [Required]
+    public string? Dni { get; set;}
+    [Required]
+    public string? Domicilio { get; set;}
+
 }
