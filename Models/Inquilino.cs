082d5fe061f0d59264561a00778a1e008bfeb7ab
ValentinaFer <quevedoants@gmail.com>
2024-03-24 05:21:43 -0300

    Added editing/creating form for Inquilinos

--- Models/Inquilino.cs
+++ Models/Inquilino.cs
@@ -7,18 +7,18 @@ public class Inquilino
     [Key]
     [Display(Name = "Código Int.")]
     public int Id { get; set;}
-    [Required]
+    [Required(ErrorMessage ="Por favor, ingrese el {0} del inquilino")]
     public string? Nombre{ get; set;}
-    [Required]
+    [Required(ErrorMessage ="Por favor, ingrese el {0} del inquilino")]
     public string? Apellido { get; set;}
-    [Required]
+    [Required(ErrorMessage ="Por favor, ingrese el {0} del inquilino"), EmailAddress(ErrorMessage ="Por favor, ingrese un email válido")]
     public string? Email { get; set;}
-    [Required]
+    [Required(ErrorMessage ="Por favor, ingrese el {0} del inquilino")]
     [Display(Name = "Teléfono")]
     public string? Telefono { get; set;}
-    [Required]
+    [Required(ErrorMessage ="Por favor, ingrese el {0} del inquilino")]
     public string? Dni { get; set;}
-    [Required]
+    [Required(ErrorMessage ="Por favor, ingrese el {0} del inquilino")]
     public string? Domicilio { get; set;}
 
 }
