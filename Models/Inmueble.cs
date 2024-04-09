e61dcd26fb055265e6fc543938c63d50a1f4e997
Pablo1864 <pablonpenaloza@sanluis.edu.ar>
2024-04-09 16:38:39 -0300

    i

--- Models/Inmueble.cs
+++ Models/Inmueble.cs
@@ -1,23 +1,23 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PenalozaFernandezInmobiliario.Models;
-
-[Table("Inmuebles")]
-
-public class Inmueble
+namespace PenalozaFernandezInmobiliario.Models
 {
+    [Table("Inmuebles")]
+    public class Inmueble
+    {
+        public int Id { get; set; }
+        public string? Direccion { get; set; }
+        public int Ambientes { get; set; }
+        public int Superficie { get; set; }
+        public decimal Latitud { get; set; }
+        public decimal Longitud { get; set; }
 
+        [DisplayName("Dueño")]
+        public int PropietarioId { get; set; }
 
-    public int Id { get; set; }
-    public string? Direccion { get; set; }
-    public int Ambientes { get; set; }
-    public int Superficie { get; set; }
-    public decimal Latitud { get; set; }
-    public decimal Longitud { get; set; }
-    public int PropietarioId { get; set; }
-    [ForeignKey(nameof(PropietarioId))]
-    public Propietario? Duenio { get; set; }
-    public bool Estado { get; set; }
-
-
+        [ForeignKey(nameof(PropietarioId))]
+        public Propietario? Duenio { get; set; }
+        public bool Estado { get; set; }
+    }
 }
